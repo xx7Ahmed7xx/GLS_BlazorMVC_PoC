cce499912c9b2ccc4cd1fddbe6fcff6a47fa01f3
Ahmed Ayman Mansour <ahmadahmad00790@yahoo.com>
2022-07-29 21:36:48 +0200

    Add project files.

---
+++ GLS_BlazorMVC_PoC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GLS_BlazorMVC_PoC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
